9da3ef0d4007ccad5904edd0f0bf18b30403a22c
Maplestorys1998 <jiangrunwang@163.com>
2021-12-03 17:20:00 +0000

    ss

--- src/changepswaction.php
+++ src/changepswaction.php
@@ -1,46 +1,35 @@
 <?php
-    session_start();
-    include_once('conn.php');
-    $username = $_SESSION['username'];
-    $curpsw = $_POST['curpsw'];
-    $newpsw = $_POST['newpsw'];
-    $newpsw2 = $_POST['newpsw2'];
+session_start();
+include_once('conn.php');
+$username = $_SESSION['username'];
+$curpsw = $_POST['curpsw'];
+$newpsw = $_POST['newpsw'];
+$newpsw2 = $_POST['newpsw2'];
 
 
-    $msg = "";
-    $sql1 = "SELECT Password FROM PoliceOffice WHERE Username = '$username' ;";
-    $result = mysqli_query($conn, $sql1);
-    
-    if(mysqli_num_rows($result) > 0)
-    {
-        $psw = mysqli_fetch_assoc($result)["Password"];
-        echo $psw;
-        if($psw !== $curpsw)
-        {   
-            $msg = "Current Password is incorrect";
-        }
+$msg = "";
+$sql1 = "SELECT Password FROM PoliceOffice WHERE Username = '$username' ;";
+$result = mysqli_query($conn, $sql1);
+
+if (mysqli_num_rows($result) > 0) {
+    $psw = mysqli_fetch_assoc($result)["Password"];
+    echo $psw;
+    if ($psw !== $curpsw) {
+        $msg = "Current Password is incorrect";
+    } else {
+        $sql2 = "UPDATE PoliceOffice SET Password = '$newpsw' WHERE Username = '$username';";
+        //change psw;
+        mysqli_query($conn, $sql2);
+        //verify if the password has been changed;
+        $sql3 = "SELECT * FROM PoliceOffice WHERE Username = '$username' AND Password = '$newpsw' ";
+        mysqli_query($conn, $sql3);
+        if (mysqli_num_rows($result) > 0) //success
+            $msg = "Change Password Successful!";
         else
-        {
-            $sql2 = "UPDATE PoliceOffice SET Password = '$newpsw' WHERE Username = '$username';";
-            //change psw;
-            mysqli_query($conn, $sql2);
-            //verify if the password has been changed;
-            $sql3 = "SELECT * FROM PoliceOffice WHERE Username = '$username' AND Password = '$newpsw' ";
-            mysqli_query($conn, $sql3);
-            if(mysqli_num_rows($result) > 0) //success
-                $msg = "Change Password Successful!";
-            else
-                $msg = "Fail to change Password!";
-        }
+            $msg = "Fail to change Password!";
     }
-    
-    else
-    {
-        $msg = "Fail to find current account!";
-    }
-    mysqli_close($conn);
-    header('Location: changepsw.php?msg=$msg');
-    
-    
-    
-?>
+} else {
+    $msg = "Fail to find current account!";
+}
+mysqli_close($conn);
+header('Location: changepsw.php?msg=$msg');
--- src/conn.php
+++ src/conn.php
@@ -1,17 +1,16 @@
 <?php
-    //read MySQL database information
-    $server = file_get_contents("../config/mysql.json");
-    $serverdata = json_decode($server, true);
-    $hostname = $serverdata["hostname"];
-    $username = $serverdata["username"];
-    $password = $serverdata["password"];
-    $dbname = $serverdata["dbname"];
-    
-    //connect to the database
-    
-    $conn = mysqli_connect($hostname, $username, $password, $dbname);
-    if(mysqli_connect_errno())
-    {
-        echo "Fail to connect MySQL";
-        die();
-    }
+//read MySQL database information
+$server = file_get_contents("../config/mysql.json");
+$serverdata = json_decode($server, true);
+$hostname = $serverdata["hostname"];
+$username = $serverdata["username"];
+$password = $serverdata["password"];
+$dbname = $serverdata["dbname"];
+
+//connect to the database
+
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
+if (mysqli_connect_errno()) {
+    echo "Fail to connect MySQL";
+    die();
+}
