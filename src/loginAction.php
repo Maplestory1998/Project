7d8e5516fb73ffa0312d30931c1fbd42be4caabc
Maplestorys1998 <jiangrunwang@163.com>
2021-12-11 12:25:23 +0000

    dsa

---
+++ src/loginAction.php
@@ -0,0 +1,29 @@
+<?php
+    include_once('conn.php');
+
+    $uname = $_POST['username'];
+    $psw = $_POST['psw'];
+
+    $sqlCheckLog = "SELECT Password FROM PoliceOffice WHERE Username = '$uname' AND Password = '$psw'; " ;
+    
+    $resultCheckLog = mysqli_query($conn, $sqlCheckLog);
+
+    if(mysqli_num_rows($resultCheckLog) > 0)
+    {
+        session_start();   
+        $_SESSION['username'] = $uname ;
+        $_SESSION['role'] = mysqli_fetch_assoc($resultCheckLog)['role']; // 1-admin ;
+        mysqli_close($conn);
+
+        header('Location: index.php');
+    }
+    else
+    {
+        $msg="Invalid Username or Password!";
+        mysqli_close($conn);
+        header("Location: login.php?msg=$msg");
+    }
+    exit;
+
+?>
+
